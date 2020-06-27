b420fe1245b8eae7d0a0b1917fd23556b60572cc
HolyMolyFoley23 <44344566+HolyMolyFoley23@users.noreply.github.com>
2020-06-27 15:53:45 -0500

    Add files via upload

---
+++ returnString/1.0.0/src/app.py
@@ -0,0 +1,32 @@
+import socket
+import asyncio
+import time
+import random
+import json
+
+
+from walkoff_app_sdk.app_base import AppBase
+
+class Tools(AppBase):
+    """
+    An example of a Walkoff App.
+    Inherit from the AppBase class to have Redis, logging, and console logging set up behind the scenes.
+    """
+    __version__ = "1.0.0"
+    app_name = "Jack Test"  # this needs to match "name" in api.yaml for WALKOFF to work
+
+    def __init__(self, redis, logger, console_logger=None):
+        """
+        Each app should have this __init__ to set up Redis and logging.
+        :param redis:
+        :param logger:
+        :param console_logger:
+        """
+        super().__init__(redis, logger, console_logger)
+
+
+    async def ret_str(self, item):
+        return str(item)
+
+if __name__ == "__main__":
+    asyncio.run(Tools.run(), debug=True)
